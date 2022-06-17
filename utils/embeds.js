c1a19f0949a0f4db081d2f53251a4032a9628a7f
RikkaHasCookies <imhunter_@outlook.com>
2022-06-16 22:05:05 -0700

    creado utils embeds

---
+++ utils/embeds.js
@@ -0,0 +1,19 @@
+const {MessageEmbed} = require('discord.js');
+
+const getCurrentSong = (guildQueue) => {
+    const ProgressBar = guildQueue.createProgressBar();
+    return embed = new MessageEmbed()
+        .setColor(process.env.BOT_COLOR)
+        .setAuthor({
+            name: '💿 Reproduciendo'
+        })
+        .setTitle(`${guildQueue.nowPlaying.name}`)
+        .setURL(`${guildQueue.nowPlaying.url}`)
+        .setThumbnail(`${guildQueue.nowPlaying.thumbnail}`)
+        .addField('Pedida por', `<@${guildQueue.nowPlaying.requestedBy}>`)
+        .addField('Duración', `[${ProgressBar.bar}] \`${ProgressBar.times}\``);
+}
+
+module.exports = {
+    getCurrentSong,
+}
