53b7d63767f47c6212ba059a920248b8af3d8a24
RikkaHasCookies <imhunter_@outlook.com>
2022-06-16 22:04:55 -0700

    creado módelo song

---
+++ models/song.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const schema = new Schema({
+    guild: {
+        type: String,
+        required: true
+    },
+    userID: {
+        type: String,
+        required: true
+    },
+    metadata: {
+        title: {
+            type: String
+        },
+        url: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: String
+        },
+    },
+    played: {
+        type: Number,
+        default: 1
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = model('song', schema);
