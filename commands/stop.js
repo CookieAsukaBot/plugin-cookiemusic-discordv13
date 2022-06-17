254677fb78fd024700682d6c6fe45d73f831312a
RikkaHasCookies <imhunter_@outlook.com>
2022-06-16 22:38:10 -0700

    se creó el comando stop

---
+++ commands/stop.js
@@ -0,0 +1,26 @@
+const {MessageEmbed} = require('discord.js');
+const {getGuildQueue} = require('../controller/music.controller');
+
+module.exports = {
+	name: 'stop',
+    category: 'Música',
+	description: 'Detiene la lista de reproducción.',
+	aliases: ['disconnect'],
+	async execute (message, args, bot) {
+        let queue = await getGuildQueue(message.guild.id, bot);
+
+        let embed = new MessageEmbed()
+            .setColor(process.env.BOT_COLOR)
+            .setAuthor({
+                name: '💥 Expulsado'
+            })
+            .setDescription(`La lista se termina, **${message.author.username}** me sacó. 💢`);
+
+        if (queue) {
+            await queue.stop();
+            message.channel.send({
+                embeds: [embed]
+            });
+        }
+	}
+}
