2ec6c22f77e951645575167165eab20011378ad1
RikkaHasCookies <imhunter_@outlook.com>
2022-06-16 22:30:41 -0700

    se creó el comando playing

---
+++ commands/playing.js
@@ -0,0 +1,20 @@
+const {getGuildQueue} = require('../controller/music.controller');
+const {getCurrentSong} = require('../utils/embeds');
+
+module.exports = {
+	name: 'playing',
+    category: 'Música',
+	description: 'Info. de la canción que se está reproduciendo.',
+    aliases: ['nowplaying', 'currentsong', 'shazam'],
+	async execute (message, args, bot) {
+        let queue = await getGuildQueue(message.guild.id, bot);
+
+        if (queue) {
+            message.channel.send({
+                embeds: [getCurrentSong(queue)]
+            });
+        } else {
+            message.channel.send(`**${message.author.username}**, no hay ninguna canción reproduciéndose.`);
+        }
+	}
+}
